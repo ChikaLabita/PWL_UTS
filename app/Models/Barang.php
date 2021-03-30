021c777ed2614142ded2637d2b8b771a92c72d7f
= <=>
2021-03-30 21:43:54 +0700

    PWL-UTS : Edit Code Model Barang

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Barang extends Model
 {
     use HasFactory;
+    protected $table='barangs'; //Eloquent akan membuat model Mahasiswa menyimpan record ditabel mahasiswas
+    public $timestamps= false;
+    protected $primaryKey = 'id_barang'; // Memanggil isi DB dengan primaryKey
+    protected $fillable= [
+        'kode_barang',
+        'nama_barang',
+        'kategori_barang',
+        'harga',
+        'qty'
+    ];
+
 }
